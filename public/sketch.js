5fff52317bfef4ffd109716c05d5b0c70acd676f
Pheobe Sun <pheobe.sun@gmail.com>
2020-03-04 12:46:32 +0000

    final change front-end

--- public/sketch.js
+++ public/sketch.js
@@ -1,33 +1,35 @@
 const screens = [];
 
-// test training samples
-const training_samples = ['a.wav', 'b.wav'];
+// training samples
+const training_samples = [
+    'test_1.wav',  'test_2.wav',
+    'test_3.wav',  'test_4.wav',
+    'test_5.wav',  'test_6.wav',
+    'test_7.wav',  'test_8.wav',
+    'test_9.wav',  'test_10.wav',
+    'test_11.wav', 'test_12.wav',
+];
 
-// const test_samples = [
-//     'test_1.wav',  'test_8.wav',
-//     'test_2.wav',  'test_18.wav',
-//     'test_3.wav',  'test_10.wav',
-//     'test_14.wav', 'test_20.wav',
-//     'test_19.wav', 'test_4.wav',
-//     'test_5.wav',  'test_6.wav',
-//     'test_13.wav', 'test_17.wav',
-//     'test_12.wav', 'test_15.wav',
-//     'test_7.wav',  'test_16.wav',
-//     'test_11.wav', 'test_9.wav'
-// ];
-
-// test samples
 const test_samples = [
-    'test_1.wav',  'test_8.wav'
+    'test_1.wav',  'test_2.wav',
+    'test_3.wav',  'test_4.wav',
+    'test_5.wav',  'test_6.wav',
+    'test_7.wav',  'test_8.wav',
+    'test_9.wav',  'test_10.wav',
+    'test_11.wav', 'test_12.wav',
+    'test_13.wav', 'test_14.wav',
+    'test_15.wav', 'test_16.wav',
+    'test_17.wav', 'test_18.wav',
+    'test_19.wav', 'test_20.wav'
 ];
 
 // Welcome page - tested
-// const welcome = new ScreenUIElements(
-//     new UIElementHTML('hint', "<h1>Instructions</h1>"),
-//     new UIElementHTML('hint', "<h3>This is a test for an audio quality study.</h3>"),
-//     new UIElementHTML('hint', "The whole test takes about <b>10-15 minutes</b> to finish.<br>You will go through a <b>training session</b> before the actual test.<br>Click 'Next' to start the training session."),
-// );
-// screens.push(welcome);
+const welcome = new ScreenUIElements(
+    new UIElementHTML('hint', "<h1>Instructions</h1>"),
+    new UIElementHTML('hint', "<h3>This is a test for an audio quality study.</h3>"),
+    new UIElementHTML('hint', "The whole test takes about <b>10-15 minutes</b> to finish.<br>You will go through a <b>training session</b> before the actual test.<br>Click 'Next' to start the training session."),
+);
+screens.push(welcome);
 
 // Training page
 var trainingCount = 0;
@@ -37,8 +39,8 @@ for (let i = 0; i < training_samples.length ; i++){
         new UIElementHTML('hint', "<h1>Training Session</h1>"),
         new UIElementHTML('hint', 'Training '+ trainingCount +' out of '+ training_samples.length),
         new UIElementHTML('hint', "1. Click 'Play' to start the audio.<br>You may replay it if you hit 'Play' again."),
-        new QuestionnaireItemMediaAudioRepeatable(undefined, '', false, `media/training/` + training_samples[i], true, "Play"),
-        new UIElementHTML('hint', "2. Answer the question below."),
+        new QuestionnaireItemMediaAudioRepeatable(undefined, '', false, `/media/training/` + training_samples[i], true, "Play"),
+        new UIElementHTML('hint', "2. Answer the question below. You cannot change your answer after you click 'Next'."),
         new QuestionnaireItemDefinedOne(undefined, "<h3>Rate the quality of the audio<br></h3>", true, ["Bad", "Poor", "Fair", "Good", "Excellent"])
     );
     screens.push(training);
@@ -72,7 +74,7 @@ for (let i = 0; i < test_samples.length; i++) {
     const test = new ScreenUIElements(
         new UIElementHTML('session', "<h1>Session 1 out of 4</h1>"),
         new UIElementHTML('session', 'Test '+ testcountShown +' out of '+test_samples.length),
-        new QuestionnaireItemMediaAudioRepeatable('session', '', false, `media/session1/`+test_samples[i], true, "Play"),
+        new QuestionnaireItemMediaAudioRepeatable('session', '', false, `media/test-1/`+test_samples[i], true, "Play"),
         new QuestionnaireItemDefinedOne('scale', '<h3>Rate the quality of the audio:</h3>', true, ["Bad", "Poor", "Fair", "Good", "Excellent"])
     );
     screens.push(test);
@@ -91,7 +93,7 @@ for (let i = 0; i < test_samples.length; i++) {
     const test = new ScreenUIElements(
         new UIElementHTML('session', "<h1>Session 2 out of 4</h1>"),
         new UIElementHTML('session', 'Test '+ testcountShown +' out of '+test_samples.length),
-        new QuestionnaireItemMediaAudioRepeatable('session', '', false, `media/session2/`+test_samples[i], true, "Play"),
+        new QuestionnaireItemMediaAudioRepeatable('session', '', false, `media/test-2/`+test_samples[i], true, "Play"),
         new QuestionnaireItemDefinedOne('scale', '<h3>Rate the quality of the audio:</h3>', true, ["Bad", "Poor", "Fair", "Good", "Excellent"])
     );
     screens.push(test);
@@ -110,7 +112,7 @@ for (let i = 0; i < test_samples.length; i++) {
     const test = new ScreenUIElements(
         new UIElementHTML('session', "<h1>Session 3 out of 4</h1>"),
         new UIElementHTML('session', 'Test '+ testcountShown +' out of '+test_samples.length),
-        new QuestionnaireItemMediaAudioRepeatable('session', '', false, `media/session3/`+test_samples[i], true, "Play"),
+        new QuestionnaireItemMediaAudioRepeatable('session', '', false, `media/test-3/`+test_samples[i], true, "Play"),
         new QuestionnaireItemDefinedOne('scale', '<h3>Rate the quality of the audio:</h3>', true, ["Bad", "Poor", "Fair", "Good", "Excellent"])
     );
     screens.push(test);
@@ -129,7 +131,7 @@ for (let i = 0; i < test_samples.length; i++) {
     const test = new ScreenUIElements(
         new UIElementHTML('session', "<h1>Session 4 out of 4</h1>"),
         new UIElementHTML('session', 'Test '+ testcountShown +' out of '+test_samples.length),
-        new QuestionnaireItemMediaAudioRepeatable('session', '', false, `media/session4/`+test_samples[i], true, "Play"),
+        new QuestionnaireItemMediaAudioRepeatable('session', '', false, `media/test-4/`+test_samples[i], true, "Play"),
         new QuestionnaireItemDefinedOne('scale', '<h3>Rate the quality of the audio:</h3>', true, ["Bad", "Poor", "Fair", "Good", "Excellent"])
     );
     screens.push(test);
